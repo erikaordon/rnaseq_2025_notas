fbe602ca98f71f698b534977199514a6aad100b7
erikaordon <erikaog@lcg.unam.mx>
2025-01-29 16:12:32 -0600

    Ejercicio de los paquetes de bioconductor

---
+++ R/paquetes_bioconductor.R
@@ -0,0 +1,17 @@
+## Nombre del paquete: HuBMAPR
+
+El paquete ofrece un atlas del cuerpo humano, que resulta interesante por la de estudios que existen relacionados y enfocados a los humanos, especialmente a un nivel tan importante como es el biomolecular y celular. Esto con la intención de descubrir y obtener datos de tejidos celulares que pueden ser útiles para su análisis. 
+
+Tiene una sola cita hasta el momento. 
+
+Contiene todas las pruebas aprobadas en los sistemas operativos. 
+
+Debido a que es un paquete nuevo, todavía no cuenta con preguntas ni respuestas. 
+
+Contiene su página HTML para la guía del usuario, misma que es conveniente revisar y esta muy bien hecha, dentro de esta se encuentra un enlace una página del mismo paquete HuBMAP que muestra de una manera más atractiva el contenido y las opciones de ejecución con R. 
+
+Parece que el paquete recientemente desarrollado es muy prometedor para el descubrimiento de tejidos.
+
+Además, parece no tener ningún problema en su ejecución. 
+
+El enlace del paquete es: https://bioconductor.org/packages/release/bioc/html/HuBMAPR.html
