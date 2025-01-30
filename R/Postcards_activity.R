e3ecec24025281c0d6c2e100fbd4a0ccda7672a7
erikaordon <erikaog@lcg.unam.mx>
2025-01-30 12:23:00 -0600

    Postcard

---
+++ R/Postcards_activity.R
@@ -0,0 +1,6 @@
+## Puedes instalar Postcards con el siguiente comando:
+install.packages("postcards")
+
+## O puedes instalar la última versión en desarrollo:
+remotes::install_github("seankross/postcards@main")
+
